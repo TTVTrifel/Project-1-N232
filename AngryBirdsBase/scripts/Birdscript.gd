1f2d8fdbfb3ca928cd4aa3e2543c1b3cf58f1ef2
JackL <129345175+TTVTrifel@users.noreply.github.com>
2024-02-28 13:45:01 -0500

    Everything

--- AngryBirdsBase/scripts/Birdscript.gd
+++ AngryBirdsBase/scripts/Birdscript.gd
@@ -8,9 +8,13 @@ var startTimer = false
 var timer = 0
 
 
+@export var stream: AudioStream
+
+
 func _ready():
 	transform = start
-
+	max_contacts_reported = 3
+	
 func _input(evt):
 	if( evt is InputEventMouseButton and evt.button_index == MOUSE_BUTTON_LEFT):
 		if(startTimer == false):
@@ -25,21 +29,56 @@ func _input(evt):
 				startTimer = true
 			
 func _process(delta):
+	Next()
 	if(startTimer == true):
 		timer = timer + 1 * delta
 	if(linear_velocity <= Vector2(10,10)):
-		if(timer > 10):
-			reset = true
+		if(reset == false):
+			if(timer > 5):
+				reset = true
+				print("reset")
 			
 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
 	if(reset == true):
-		linear_velocity = Vector2(0,0)
-		transform = start
-		reset = false
 		timer = 0
 		startTimer = false
 
+func _integrate_forces(state):
+	if(reset == true):
+		print("oof")
+		state.transform = start
+		linear_velocity = Vector2(0,0)
+		reset = false
 
 
+func Next():
+	
+	await(get_tree().create_timer(2))
+	if(GameManager.score == 1 and GameManager.level == 0):
+		print("mama")
+		get_tree().change_scene_to_file("res://scenes/game2.tscn")
+		GameManager.level = 1
+	if(GameManager.score == 2 and GameManager.level == 1):
+		get_tree().change_scene_to_file("res://scenes/game3.tscn")
+		GameManager.level = 2
+	if(GameManager.score == 3 and GameManager.level == 2):
+		GameManager.level = 0
+		GameManager.score = 0
+		
+		get_tree().change_scene_to_file("res://scenes/End.tscn")
+
+
+
+
+
+
+
+
+
+
+
+
+func _on_body_entered(body):
+	AudioManager.play_sound(stream)
--- AngryBirdsBase/scripts/Sus.gd
+++ AngryBirdsBase/scripts/Sus.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
 #TODO: You'll probably need an export_var for the particle effect here
-
+@export var deathParticle: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,4 +29,14 @@ func _on_body_entered(body):
 	if(body.is_in_group("Player")):
 		print("hi")
 		GameManager.score = GameManager.score + 1
-		
+		await(get_tree().create_timer(1))
+		Kill()
+
+func Kill():
+	var Particle = deathParticle.instantiate()
+	Particle.position = global_position
+	Particle.rotation = global_rotation
+	Particle.emitting = true
+	get_tree().current_scene.add_child(Particle)
+	queue_free()
+	
